8a5e612c136e06e0d0a269cdc113f8430368ec19
Oliver Brook <olibrook@gmail.com>
2023-11-12 13:45:14 -0800

    WIP

--- web/src/server/api/root.ts
+++ web/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "@buds/server/api/routers/post";
 import { createTRPCRouter } from "@buds/server/api/trpc";
+import {statRouter} from "@buds/server/api/routers/stat";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "@buds/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  stat: statRouter,
 });
 
 // export type definition of API
