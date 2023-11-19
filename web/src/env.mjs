a4f428c63aef3d55fe82089275d769905c337786
Oliver Brook <olibrook@gmail.com>
2023-11-18 16:09:16 -0800

    Okee, now ya can write tests against the db

--- web/src/env.mjs
+++ web/src/env.mjs
@@ -45,6 +45,7 @@ export const env = createEnv({
      * middlewares) or client-side so we need to destruct manually.
      */
     runtimeEnv: {
+        TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,16 +1,26 @@
 // See https://github.com/aiji42/vitest-environment-vprisma/tree/main
 
+import path from "path";
 import { defineConfig } from "vite";
+import "dotenv/config";
+
+if (!process.env.TEST_DATABASE_URL) {
+  throw new Error("TEST_DATABASE_URL required in env");
+}
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@buds": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
     environment: "vprisma",
     setupFiles: ["vitest-environment-vprisma/setup", "vitest.setup.ts"],
-    environmentOptions: {
-      vprisma: {
-        databaseUrl: process.env.TEST_DATABASE_URL,
-      },
+    env: {
+      ...process.env,
+      DATABASE_URL: process.env.TEST_DATABASE_URL,
     },
   },
 });
