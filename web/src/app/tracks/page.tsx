e1147caf473ac1849bb93356bbe66399fd40a9a6
Oliver Brook <olibrook@gmail.com>
2023-11-28 19:57:31 -0800

    WIP

--- web/src/app/tracks/page.tsx
+++ web/src/app/tracks/page.tsx
@@ -9,7 +9,7 @@ import {
   subDays,
 } from "date-fns";
 import { RouterOutputs } from "@buds/trpc/shared";
-import { ReactNode, useContext, useMemo } from "react";
+import { useContext, useMemo } from "react";
 import AppShell, {
   Measurement,
   UserSettingsContext,
@@ -161,79 +161,37 @@ const Avatar: React.FC<AvatarProps> = ({ imageUrl, userName, size }) => {
   );
 };
 
-const Header = ({ keyGroup, level }: { keyGroup: KeyGroup; level: 0 | 1 }) => {
-  let icon: ReactNode = <></>;
-  let name = "";
-  let key = "";
-  const size = level == 0 ? "lg" : "md";
-  const colSpan = keyGroup.childKeys.length;
+const KeyGroupIcon = ({
+  keyGroup,
+  size,
+}: {
+  keyGroup: KeyGroup;
+  size: "md" | "lg";
+}) => {
   switch (keyGroup.sortKey.kind) {
     case "track":
       const track = keyGroup.sortKey.track;
-      name = track.name;
-      key = track.name;
-      icon = <Icon fallback={trackIcons[track.name] ?? "?"} size={size} />;
-      break;
+      return <Icon fallback={trackIcons[track.name] ?? "?"} size={size} />;
+
     case "user":
       const user = keyGroup.sortKey.user;
-      name = user.name ?? "Anon";
-      key = user.id;
-      icon = <Avatar imageUrl={user.image} userName={name} size={size} />;
-      break;
-  }
-
-  const classes = "font-medium";
-
-  if (level === 0) {
-    // Big boy
-    return (
-      <th key={key} colSpan={colSpan} className={classes}>
-        <div className="flex flex-row items-center justify-start py-2 pl-1">
-          {icon}
-          <span className="ml-2">{name}</span>
-        </div>
-      </th>
-    );
-  } else {
-    // Little guy
-    return (
-      <th key={key} colSpan={colSpan} className={classes}>
-        <div className="flex items-center justify-center">{icon}</div>
-      </th>
-    );
+      return (
+        <Avatar
+          imageUrl={user.image}
+          userName={user.name ?? "Anon"}
+          size={size}
+        />
+      );
   }
 };
 
-const Headers = ({
-  level,
-  keyGroups,
-}: {
-  level: 0 | 1;
-  keyGroups: KeyGroup[];
-}) => {
-  const children: ReactNode[] = [];
-
-  if (level === 0) {
-    keyGroups.forEach((keyGroup, idx) => {
-      const isLast = idx === keyGroups.length - 1;
-      children.push(<Header keyGroup={keyGroup} level={level} />);
-      if (!isLast) {
-        children.push(<Spacer key={`spacer-${idx}`} type="th" />);
-      }
-    });
-  } else if (level === 1) {
-    keyGroups.forEach((outerKeyGroup, idx) => {
-      const isLast = idx === keyGroups.length - 1;
-      outerKeyGroup.childKeys.forEach((keyGroup) => {
-        children.push(<Header keyGroup={keyGroup} level={level} />);
-      });
-      if (!isLast) {
-        children.push(<Spacer key={`spacer-${idx}`} type="th" />);
-      }
-    });
+const KeyGroupName = ({ keyGroup }: { keyGroup: KeyGroup }) => {
+  switch (keyGroup.sortKey.kind) {
+    case "track":
+      return keyGroup.sortKey.track.name;
+    case "user":
+      return keyGroup.sortKey.user.name ?? "Anon";
   }
-
-  return <>{children}</>;
 };
 
 export default function Home() {
@@ -307,52 +265,42 @@ export function TrackList() {
   const numRows = differenceInDays(stats.end, stats.start) - 1;
   const rowsMap = Array.from(new Array(numRows));
 
+  const selectedKeyGroup = keyGroups[0];
+
+  if (!selectedKeyGroup) {
+    return null;
+  }
+
   return (
     <main className={`h-screen px-4 pb-16 pt-12 font-light text-gray-600`}>
-      <div className="h-full w-full rounded-xl bg-[#7371b5] py-4 shadow-xl drop-shadow-xl">
+      <div className="h-full w-full rounded-xl bg-[#7371b5] pb-4 shadow-xl drop-shadow-xl">
+        <div className="flex items-center justify-end p-4 text-white">
+          <KeyGroupName keyGroup={selectedKeyGroup} />
+          <span className="ml-4">
+            <KeyGroupIcon keyGroup={selectedKeyGroup} size="lg" />
+          </span>
+        </div>
         <div className="h-full w-full overflow-scroll bg-gray-50">
           <table className="">
             <thead className="sticky top-0 bg-gray-50">
-              <tr className="bg-[#7371b5] text-white">
-                <th></th>
-                <Headers level={0} keyGroups={keyGroups} />
-              </tr>
-              <tr>
-                <th className="text-right font-normal">Nov 23</th>
-                <Headers level={1} keyGroups={keyGroups} />
+              <tr className="font-normal">
+                <th className="py-2 text-right">Nov 23</th>
+                {selectedKeyGroup.childKeys.map((kg) => (
+                  <th
+                    className="py-2"
+                    key={`${kg.sortKey.user.id}-${kg.sortKey.track.name}`}
+                  >
+                    <div className="flex items-center justify-center">
+                      <KeyGroupIcon keyGroup={kg} size="md" />
+                    </div>
+                  </th>
+                ))}
               </tr>
             </thead>
             <tbody>
               {rowsMap.map((_, dateOffset) => {
                 const date = subDays(stats.end, dateOffset);
                 const isWeekend = isSaturday(date) || isSunday(date);
-                const children: ReactNode[] = [];
-
-                keyGroups.forEach((outerKeyGroup, i) => {
-                  const last = i === keyGroups.length - 1;
-                  outerKeyGroup.childKeys.forEach((keyGroup) => {
-                    const sk = keyGroup.sortKey;
-                    const value = accessor(
-                      stats,
-                      sk.user.id,
-                      sk.track.name,
-                      dateOffset,
-                    );
-                    // TODO: Make a TD component
-                    children.push(
-                      <td
-                        key={`${sk.user.id}-${sk.track.name}`}
-                        className={`h-[45px] min-w-[50px] text-center`}
-                      >
-                        <CellValue trackName={sk.track.name} value={value} />
-                      </td>,
-                    );
-                  });
-                  if (!last) {
-                    children.push(<Spacer key={`spacer-${i}`} type="td" />);
-                  }
-                });
-
                 return (
                   <tr
                     key={`${dateOffset}`}
@@ -361,7 +309,23 @@ export function TrackList() {
                     <td className="min-w-[70px] px-2 text-right text-sm">
                       {formatDate(date)}
                     </td>
-                    {children}
+                    {selectedKeyGroup.childKeys.map((keyGroup) => {
+                      const sk = keyGroup.sortKey;
+                      const value = accessor(
+                        stats,
+                        sk.user.id,
+                        sk.track.name,
+                        dateOffset,
+                      );
+                      return (
+                        <td
+                          key={`${sk.user.id}-${sk.track.name}`}
+                          className={`h-[45px] min-w-[50px] text-center`}
+                        >
+                          <CellValue trackName={sk.track.name} value={value} />
+                        </td>
+                      );
+                    })}
                   </tr>
                 );
               })}
