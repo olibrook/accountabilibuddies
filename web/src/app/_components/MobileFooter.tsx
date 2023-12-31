2902de3fb070cc0fadd2ae1bda7a8f208516824b
Oliver Brook <olibrook@gmail.com>
2023-12-31 13:33:30 +0000

    Rearrange footer links

--- web/src/app/_components/MobileFooter.tsx
+++ web/src/app/_components/MobileFooter.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import { Heart, Home, PlusSquare, Search, User } from "react-feather";
+import { Search, Settings, User, Users } from "react-feather";
+import Link from "next/link";
 
 const MobileFooter = () => {
   return (
     <div className="fixed bottom-0 flex w-full items-center justify-around bg-white py-4">
       <div className="text-gray-500 hover:text-black">
-        <Home size={20} />
+        <Link href="/users/me">
+          <User size={20} />
+        </Link>
+      </div>
+      <div className="text-gray-500 hover:text-black">
+        <Link href="/tracks/first">
+          <Users size={20} />
+        </Link>
       </div>
       <div className="text-gray-500 hover:text-black">
         <Search size={20} />
       </div>
       <div className="text-gray-500 hover:text-black">
-        <PlusSquare size={20} />
-      </div>
-      <div className="text-gray-500 hover:text-black">
-        <Heart size={20} />
-      </div>
-      <div className="text-gray-500 hover:text-black">
-        <User size={20} />
+        <Settings size={20} />
       </div>
     </div>
   );
