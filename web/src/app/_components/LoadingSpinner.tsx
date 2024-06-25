49f47836245308705590df18a47eb6206954b142
Oliver Brook <olibrook@gmail.com>
2024-06-24 21:56:10 -0700

    Nicer home screen

---
+++ web/src/app/_components/LoadingSpinner.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { FaSpinner } from "react-icons/fa";
+
+export const LoadingSpinner = () => {
+  return <FaSpinner className="h-8 w-8 animate-spin text-white opacity-50" />;
+};
